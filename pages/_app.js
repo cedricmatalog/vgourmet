2361ed04f1fe6e0cb3bdda175e00c416c483c8ac
Cedric Matalog <cedricmatalog@gmail.com>
2020-07-31 21:11:00 +0800

    setups basic pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,54 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import {
+  Navbar,
+  Nav,
+  NavDropdown,
+  Form,
+  FormControl,
+  Button,
+  Container,
+} from "react-bootstrap";
+import styles from "../styles/Home.module.css";
+import Link from "next/link";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  const { Brand, Toggle, Collapse } = Navbar;
+
+  return (
+    <div className={styles.home}>
+      <Navbar bg="light" expand="lg">
+        <Container>
+          <Brand href="#home">V Gourmet</Brand>
+          <Toggle aria-controls="basic-navbar-nav" />
+          <Collapse id="basic-navbar-nav">
+            <Nav className="ml-auto">
+              <Link href="/">
+                <Nav.Link href="#home">Home</Nav.Link>
+              </Link>
+              <Link href="/about_us">
+                <Nav.Link href="#link">About us</Nav.Link>
+              </Link>
+
+              <Link href="/products">
+                <Nav.Link href="#link">Products</Nav.Link>
+              </Link>
+
+              <Link href="/contact_us">
+                <Nav.Link href="#link">Contact us</Nav.Link>
+              </Link>
+
+              <Link href="/testimonials">
+                <Nav.Link href="#link">Testimonials</Nav.Link>
+              </Link>
+            </Nav>
+          </Collapse>
+        </Container>
+      </Navbar>
+      <Container className="flex-grow-1 d-flex align-items-center">
+        <Component {...pageProps} />
+      </Container>
+    </div>
+  );
 }
 
-export default MyApp
+export default MyApp;
